7ff528efd8aa34577935f29a0efb40278a7a3ccc
arisfatahillah <arisfatahillah@gmail.com>
2023-06-14 16:54:15 +0700

    add page object for login

---
+++ src/main/java/org/saucedemo/pages/LoginPage.java
@@ -0,0 +1,38 @@
+package org.saucedemo.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage {
+    private WebDriver driver;
+
+    // Element locator from login page
+    private By usernameField = By.id("user-name");
+    private By passwordField = By.id("password");
+    private By loginButton = By.id("login-button");
+
+    // Constructor
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void enterUsername(String username) {
+        WebElement usernameInput = driver.findElement(usernameField);
+    }
+
+    public void enterPassword(String password) {
+        WebElement passwordInput = driver.findElement(passwordField);
+    }
+
+    public void clickButton(String buttonName) {
+        WebElement buttonTextName = driver.findElement(By.linkText(buttonName));
+        buttonTextName.click();
+    }
+
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickButton("Login");
+    }
+}
---
+++ src/test/java/org/saucedemo/stepdef/LoginStepDef.java
@@ -0,0 +1,4 @@
+package org.saucedemo.stepdef;
+
+public class LoginStepDef {
+}
